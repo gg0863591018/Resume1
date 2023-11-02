b40d2d255493ca59b6729a74648eb2761d286c86
gg0863591018 <149705213+gg0863591018@users.noreply.github.com>
2023-11-03 00:17:19 +0700

    Add files via upload

---
+++ Get2.js
@@ -0,0 +1,8 @@
+function mobile (){
+    if(document.querySelector('h2').innerHTML === '0972096357'){
+        document.querySelector('h2').innerHTML = 'Telephone';
+}
+else {
+    document.querySelector('h2').innerHTML = '0972096357';
+}
+}
